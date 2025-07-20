5d824fcc4c0e063b84ae97165bf0f8a0d833adde
rm40405 <rm404085@gmail.com>
2025-07-20 17:27:03 +0600

    part4 part 5 complete

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -3,6 +3,44 @@ import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-go
 import { envVars } from "./env";
 import { User } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interfaces";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs";
+
+
+passport.use(
+    new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    }, async (email: string, password: string, done) => {
+        try {
+            const isUserExist = await User.findOne({ email })
+
+            
+
+            if (!isUserExist) {
+                return done(null, false, { message:"User does not exist" })
+            }
+
+               const isGoogleAuthenticated = isUserExist.auth.some(providerObjects => providerObjects.provider == "google")
+
+            if (isGoogleAuthenticated) {
+                return done(null, false, { message: "You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password." })
+            }
+
+           const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+
+            if (!isPasswordMatched) {
+                return done(null, false, { message: "Password does not match" })
+            }
+
+            return done(null, isUserExist)
+
+        } catch (error) {
+            console.log(error);
+            done(error)
+        }
+    })
+)
 
 passport.use(
     new GoogleStrategy (
@@ -50,6 +88,12 @@ passport.use(
         }
     )
 )
+
+// frontend localhost:5173/login?redirect=/booking -> localhost:5000/api/v1/auth/google?redirect=/booking -> passport -> Google OAuth Consent -> gmail login -> successful -> callback url localhost:5000/api/v1/auth/google/callback -> db store -> token
+
+// Bridge == Google -> user db store -> token
+//Custom -> email , password, role : USER, name... -> registration -> DB -> 1 User create
+//Google -> req -> google -> successful : Jwt Token : Role , email -> DB - Store -> token - api access
 
 
 passport.serializeUser((user: any, done: (err: any, id?:unknown) =>void)=>{
---
+++ src/app/modules/division/division.route.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { checkAuth } from "../../middleware/checkauth";
+import { Role } from "../user/user.interfaces";
+import { createDivisionSchema, updateDivisionSchema } from "./validationSchema";
+import { DivisionController } from "./division.controller";
+import { validationRequest } from "../../globalError/validationRequest";
+
+
+
+const router = Router()
+
+router.post(
+    "/create",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validationRequest(createDivisionSchema),
+    DivisionController.createDivision
+);
+router.get("/", DivisionController.getAllDivisions);
+router.get("/:slug", DivisionController.getSingleDivision)
+router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validationRequest(updateDivisionSchema),
+    DivisionController.updateDivision
+);
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), DivisionController.deleteDivision);
+
+export const DivisionRoutes = router
